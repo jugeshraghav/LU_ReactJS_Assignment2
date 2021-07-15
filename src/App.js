04578c290f09454e7328c651f6bd06ffd48e0488
Jugesh Raghav <jugesh1520raghav@gmail.com>
2021-07-15 11:01:39 +0530

    LU ReactJS Assignment2 13/07/21

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=>{
+    const [count, setCount]= useState(0);
+return(
+    <div>
+    <h1>COUNTER APP</h1> 
+    <h3>The value of counter is {count}</h3> 
+<button onClick={()=>count<25 ? setCount(count+1) : "" }>Increament</button>
+<button onClick={()=>count>0 ? setCount (count-1) : ""}>Decreament</button>
+<button onClick={()=>{ setCount(0)}}>Reset</button>
+</div>
+)
+ 
 }
 
 export default App;
